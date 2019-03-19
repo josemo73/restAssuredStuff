187a4d00bac1d9f9e5c272159f155add16c1751d
Jose Moreno <joselomamb@gmail.com>
2019-03-18 18:42:41 -0600

    allow execution of basic RestFul api tests

--- src/test/java/com/zenith/tests/TestRestAssuredSamples.java
+++ src/test/java/com/zenith/tests/TestRestAssuredSamples.java
@@ -1,30 +1,52 @@
 package com.zenith.tests;
 
 import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class TestRestAssuredSamples {
-	
 	@Test
-	public void test_NumberOfCircuitsFor2017Season_ShouldBe20() {
-	        
+	public void requestUsZipCode90210_logRequestAndResponseDetails() {
 	    given().
+	    	log().all().
 	    when().
-	        get("http://ergast.com/api/f1/2017/circuits.json").
+	        get("https://zippopotam.us/us/90210").
 	    then().
-	        assertThat().
-	        body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
+	        log().body();
 	}
 	
 	@Test
-	public void requestUsZipCode90210_CheckPlaceNameInResponseBody_expectBeverlyHills() {
+	public void requestUsZipCode90210_checkContentType_expectJSON() {
 	    given().
 	    when().
 	        get("https://zippopotam.us/us/90210").
 	    then().
 	        assertThat().
-	        body("places[0].'place name'", equalTo("Beverly Hills"));
-	    	
+	        contentType(ContentType.JSON);
+	}
+	
+	@Test
+	public void requestUsZipCode90210_checkStatusCode_expectHTTP200() {
+	    given().
+	    when().
+	        get("https://zippopotam.us/us/90210").
+	    then().
+	        assertThat().
+	        statusCode(200);
+
+	}
+	
+	@Test
+	public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
+	    given().
+	    when().
+	        get("https://zippopotam.us/us/90210").
+	    then().
+	        assertThat().
+	        body("places[0].'place name'",  equalTo("Beverly Hills"));
+
 	}
 }
